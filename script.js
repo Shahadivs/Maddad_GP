77f480af03ca93d5574cdb60b7933bf86e4be083
laalamer <lamaalamer0@gmail.com>
2026-03-06 14:35:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function goTo(pageId) {
+  const pages = document.querySelectorAll('.page');
 
+  pages.forEach((page) => {
+    page.classList.remove('active');
+  });
+
+  document.getElementById(pageId).classList.add('active');
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+function selectRole(role) {
+  if (role === 'parent') {
+    goTo('parentLoginPage');
+  } else if (role === 'child') {
+    goTo('childLoginPage');
+  }
+}
